9952ab3c1c2871575bd223c23ec2af7f07a986d3
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-06-30 00:54:29 +0530

    Time: 0 ms (100%), Space: 8.1 MB (7.95%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
-        vector<int> arr;
-        while(num>0){
-            int remainder = num%10;
-            arr.push_back(remainder);
-            num = num/10;
-        }
+    int maximum69Number(int num) {
+        string s = to_string(num);
 
-        reverse(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==6){
-                arr[i]=9;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
                 break;
             }
         }
 
-        int ans=0;
-        for(int i=0;i<arr.size();i++){
-            ans = ans*10 + arr[i];
-        }
-        return ans;
+        return stoi(s);
     }
 };
