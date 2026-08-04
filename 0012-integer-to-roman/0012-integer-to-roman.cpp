d0cf0d5670264abd9f5e27aa438455a769c8a94f
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-08-04 23:29:42 +0530

    Time: 5 ms (38.46%), Space: 12.7 MB (18.54%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int,string>> arr = {
-            {1000,"M"}, {900,"CM"}, {500,"D"},{400,"CD"}, {100,"C"}, 
-            {90,"XC"},{50,"L"}, {40,"XL"}, {10,"X"},
-            {9,"IX"}, {5,"V"}, {4,"IV"}, {1,"I"}
+        vector<pair<int, string>> arr = {
+            {1000,"M"}, {900,"CM"}, {500,"D"}, {400,"CD"}, {100,"C"}, 
+            {90,"XC"}, {50,"L"}, {40,"XL"}, {10,"X"}, {9,"IX"}, {5,"V"}, 
+            {4,"IV"}, {1,"I"}
         };
-
-        string ans="";
-        for(auto it:arr){
-            while(num >= it.first){
-                ans += it.second;
-                num -= it.first;
+        string s = "";
+        for(auto x:arr){
+            while(num >= x.first){
+                s += x.second;
+                num -= x.first;
             }
         }
-
-        return ans;
+        return s;
     }
 };
