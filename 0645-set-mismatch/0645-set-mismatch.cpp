372d795e35997a98533f10bfb05668c4d2bb12d2
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-08-05 01:00:34 +0530

    Time: 35 ms (15.72%), Space: 34.7 MB (16.86%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (auto it : nums) {
+            mp[it]++;
+        }
+
+        int duplicate = -1, missing = -1;
+        for (int i = 1; i <= n; i++) {
+            if (mp[i] == 2) {
+                duplicate = i;
+            } else if (mp[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return {duplicate, missing};
+    }
+};
