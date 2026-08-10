3ec9c6168fa3e9acd0303e6177a7e285bc8263c0
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-08-10 13:32:32 +0530

    Time: 26 ms (44.05%), Space: 88.3 MB (73.21%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -6,26 +6,20 @@ public:
         mp[0] = -1;
         int maxlen = 0, sum = 0;
         for (int i = 0; i < n; i++) {
-            int len = 0;
             if (nums[i] == 0) {
                 sum += -1;
-
-                if (mp.find(sum) == mp.end()) {
-                    mp[sum] = i;
-                } else {
-                    len = i - mp[sum];
-                }
             } 
             else {
                 sum += 1;
-
-                if (mp.find(sum) == mp.end()) {
-                    mp[sum] = i;
-                } else {
-                    len = i - mp[sum];
-                }
             }
-            maxlen = max(maxlen, len);
+
+            if (mp.find(sum) == mp.end()) {
+                mp[sum] = i;
+            } 
+            else {
+                int len = i - mp[sum];
+                maxlen = max(maxlen, len);
+            }
         }
         return maxlen;
     }
