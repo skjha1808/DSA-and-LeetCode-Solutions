d1ae708d1ee8fdb801851e8e7809412f75ec30a4
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-09-02 09:18:48 +0530

    Time: 754 ms (57.92%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select s.product_id, f.first_year, s.quantity, s.price
+from sales s
+join (
+    select product_id, min(year) as first_year
+    from sales 
+    group by product_id
+) f
+on s.product_id = f.product_id
+and s.year = f.first_year;
