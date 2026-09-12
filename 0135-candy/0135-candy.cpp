b0ee3d6abc93753101ff559d9cae7c53f3fbb038
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-09-12 11:52:02 +0530

    Time: 2 ms (49.1%), Space: 71.4 MB (15.07%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> arr(n, 1);
+        int ans = 0;
+
+        // left -> right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        // right -> left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                arr[i] = max(arr[i], arr[i + 1] + 1);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans += arr[i];
+        }
+
+        return ans;
+    }
+};
