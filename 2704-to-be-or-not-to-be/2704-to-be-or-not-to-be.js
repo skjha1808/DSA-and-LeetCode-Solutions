031ec060629a20e7cfcf8be1b014f2ea66e6b8a3
Shubham Kumar <shubhamkumar180804@gmail.com>
2026-09-22 17:52:14 +0530

    Time: 48 ms (30%), Space: 54.5 MB (7.94%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: other => {
+            if (val === other) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: other => {
+            if (val !== other) return true;
+            throw new Error("Equal");
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
